719bd22f3f04aea8dd73111e4be5eee91c7cad05
Zotti39 <gabriel.zzanatta03@gmail.com>
2023-10-13 19:51:23 -0300

    now its possible to remake the grid without having to refresh the page, there is a button that will prompt you for the size you want the grid to be, and it will switch automatically

--- javascript.js
+++ javascript.js
@@ -1,17 +1,27 @@
 const container = document.querySelector('#container');
-const gridSize = 16;
 const botao = document.querySelector('#botao');
+let gridSize = 16;
 
-window.addEventListener("DOMContentLoaded", function(){
+// function to make the grid
+function grid(){
     for(let i = 1; i <= gridSize; i++){
         const newLine = document.createElement('div');
         newLine.setAttribute('class', 'linha');
         for(let j = 1; j <= gridSize; j++){
             const newColumn = document.createElement('div');
             newColumn.setAttribute('class', 'coluna');
-            newColumn.textContent = `Linha${i} Coluna${j}`;
+            newColumn.innerHTML = `Linha${i}<br>Coluna${j}`;
             newLine.appendChild(newColumn);
         }
         container.appendChild(newLine);
     }
+}
+//Loads the function as soon as the page loads
+window.addEventListener('DOMContentLoaded', grid);
+
+//function of the button, used to make a new grid
+botao.addEventListener("click", function(){
+    gridSize = parseInt(prompt("What size do you want the grid to be?", "Number..."));
+    container.innerHTML = "";
+    grid();
 })
