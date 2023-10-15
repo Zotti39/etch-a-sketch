4585272fe5efb9e2814500c2dea822a29b93c4ea
Zotti39 <gabriel.zzanatta03@gmail.com>
2023-10-15 13:40:59 -0300

    Function that randomize the background color of clicked boxes of the grid was added. Used a nodeList and a listener to individualize the function, some css properties were also changed

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
 const container = document.querySelector('#container');
 const botao = document.querySelector('#botao');
 let gridSize = 16;
-
-// function to make the grid
+//function that return a random color (part of the XtraCredit1)
+function randomColor(){
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+//XtraCredit1 function
+function randomBack(item){
+    const targetElement = item.target;
+    const corAleatoria = randomColor();
+    targetElement.style.cssText = `background-color: ${corAleatoria};`;
+}
+// Function to make the grid
 function grid(){
     for(let i = 1; i <= gridSize; i++){
         const newLine = document.createElement('div');
@@ -15,11 +29,15 @@ function grid(){
         }
         container.appendChild(newLine);
     }
+    //Calling de Xtra1 credit function
+    const gridArray = document.querySelectorAll(".coluna");
+    gridArray.forEach(function(item) {
+        item.addEventListener("click", randomBack);
+    });
 }
 //Loads the function as soon as the page loads
-window.addEventListener('DOMContentLoaded', grid);
-
-//function of the button, used to make a new grid
+document.addEventListener('DOMContentLoaded', grid);
+//Function of the button, used to make a new grid
 botao.addEventListener("click", function(){
     gridSize = parseInt(prompt("What size do you want the grid to be?", "Number..."));
     container.innerHTML = "";
